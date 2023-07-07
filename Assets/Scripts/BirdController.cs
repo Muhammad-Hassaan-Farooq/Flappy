b73012601c82662793c986bcff2d6598f6bd3bf5
Muhammad Hassaan Farooq <muhammadhassaanf@gmail.com>
2023-07-07 23:45:33 +0500

    Finalized
    Added Restart functionality

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -6,6 +6,9 @@ public class BirdController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D birdRigidbody2D;
     [SerializeField] private float upForce = 20f;
+    [SerializeField] private Logic logic;
+
+    private bool isDead = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +18,15 @@ public class BirdController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !isDead)
         {
             birdRigidbody2D.velocity = Vector2.up * upForce;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+        logic.gameOver();
     }
 }
--- Assets/Scripts/Logic.cs
+++ Assets/Scripts/Logic.cs
@@ -1,17 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Logic : MonoBehaviour
 {
     private int score = 0;
     [SerializeField] private Text scoreText;
+    [SerializeField] private GameObject endScreen;
 
     [ContextMenu("addScore")]
     public void addScore(int scoreToAdd)
     {
         score += scoreToAdd;
         scoreText.text = score.ToString();
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        endScreen.SetActive(true);
     }
 }
